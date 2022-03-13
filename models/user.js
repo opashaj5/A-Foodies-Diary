dbc597d25113626417110f5ffea4bb53e9c4ec64
opashaj5 <opashaj5@yahoo.com>
2022-03-12 22:58:32 -0600

    updated user schema

--- models/user.js
+++ models/user.js
@@ -3,10 +3,19 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    password: String,
-  });
+  name:{
+    type: String,
+    required:true
+  },
+  email:{
+    type: String,
+    required:true
+  },
+  password:{
+    type: String,
+    required:true
+  }
+});
 
 const User = model("user", userSchema);
 
