2fd694a0264482519ec823f4ce22e70908c9b764
opashaj5 <opashaj5@yahoo.com>
2022-03-12 19:27:57 -0600

    added footer, made updates to header, and added css

--- models/user.js
+++ models/user.js
@@ -1,24 +1,11 @@
-const mongoose = require("./connection");
+const mongoose = require("mongoose");
 
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
-    name: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    }
+    name: String,
+    email: String,
+    password: String,
   });
 
 const User = model("user", userSchema);
